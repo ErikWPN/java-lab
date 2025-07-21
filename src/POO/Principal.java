444b4c61ae096c98c39a5698507a3a1b98fbb7ea
ErikWPN <erikwesley16@gmail.com>
2025-07-21 18:06:59 -0300

    featt: Criando uma posta models para um melhor controle de objetos

--- src/POO/Principal.java
+++ src/POO/Principal.java
@@ -1,20 +1,21 @@
 package POO;
 
+import POO.models.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme primeiroFilme = new Filme();
-        primeiroFilme.nome = "Avangers Ultimato";
-        primeiroFilme.informacao = """
+        primeiroFilme.setNome("Avangers Ultimato");
+        primeiroFilme.setInformacao("""
                 Após Thanos eliminar metade das criaturas vivas,
                 os Vingadores têm de lidar com a perda de amigos e entes 
                 queridos. Com Tony Stark vagando perdido no espaço sem água
                 e comida, Steve Rogers e Natasha Romanov lideram a 
                 resistência contra o titã louco.
-                """;
-        primeiroFilme.anoDeLancamento = 2019;
-        primeiroFilme.restIdade = 12;
-        primeiroFilme.duracaoEmMinutos = 181;
-
+                """);
+        primeiroFilme.setAnoDeLancamento(2019);
+        primeiroFilme.setRestIdade(12);
+        primeiroFilme.setDuracaoEmMinutos(181);
         primeiroFilme.exibeFichaTecnica();
         primeiroFilme.avalia(10);
         primeiroFilme.avalia(9);
---
+++ src/POO/models/Filme.java
@@ -0,0 +1,86 @@
+package POO.models;
+
+public class Filme {
+    private String nome;
+    private String informacao;
+    private int anoDeLancamento;
+    private int restIdade;
+    private double somaavalicao;
+    private int totalDeNota;
+    private int duracaoEmMinutos;
+    private boolean incluidoNoPlano;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getInformacao() {
+        return informacao;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public int getRestIdade() {
+        return restIdade;
+    }
+
+    public double getSomaavalicao() {
+        return somaavalicao;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getTotalDeNota(){
+        return totalDeNota;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setInformacao(String informacao) {
+        this.informacao = informacao;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setRestIdade(int restIdade) {
+        this.restIdade = restIdade;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Faixa etária de idade: " + restIdade);
+        System.out.println("Sinopse: " + informacao);
+
+    }
+
+    public void avalia (double nota){
+        somaavalicao += nota;
+        totalDeNota++;
+    }
+
+    public double obterMedia () {
+        return somaavalicao / totalDeNota;
+    }
+}
+
